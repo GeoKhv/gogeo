625c58e54f4a2a991c4bef1abbcfb718ecf499ba
Georgii S <geokhv@mail.ru>
2024-04-05 02:24:08 +0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+    name: "gogeo",
+    script: "./server.js",
+    env: {
+      "OPENAI_API_KEY": "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"
+    }
+  }]
+};
